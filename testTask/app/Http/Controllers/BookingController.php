a9f25cc4dc0aa6ef9f452552099a8f4cd0997cf0
levifeyd <kuleshov-as@mail.ru>
2023-02-27 15:57:56 +0400

    get categoriesId

--- testTask/app/Http/Controllers/BookingController.php
+++ testTask/app/Http/Controllers/BookingController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class BookingController extends Controller
-{
+class BookingController extends Controller {
     public function getRequestFreeCars(Request $request) {
         $apiToken = $request->header('API_TOKEN');
         $user = User::query()->where('api_token', $apiToken)->first();
         if (!$user) return 'Incorrect api token';
+        $categoriesIds = $user->carsCategories()->toArray();
+        $input = json_decode($request->getContent(), true);
 
-        return $apiToken;
+        if (!$input) return 'Incorrect request';
+        if (array_key_exists("categories_id", $input)) {
+            $categoriesIds = array_intersect($categoriesIds, $input["categories_id"]);
+        }
+//        $query = Car::query()->select('id', 'brand_car', 'driver_name')
+//            ->whereIn('comfort_category', $categoriesIds);
+        return $categoriesIds;
     }
 }
