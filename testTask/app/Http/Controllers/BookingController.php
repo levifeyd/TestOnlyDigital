0bd1f761cc878ed059639b1bacef063ff6e2fdcf
levifeyd <kuleshov-as@mail.ru>
2023-02-27 13:37:35 +0400

    add route and create BookingController

---
+++ testTask/app/Http/Controllers/BookingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function getRequestFreeCars(Request $request) {
+        $apiToken = $request->header('API_TOKEN');
+        $user = User::query()->where('api_token', $apiToken)->first();
+        if (!$user) return 'Incorrect api token';
+
+    }
+}
